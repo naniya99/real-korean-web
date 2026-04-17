ecfe709e971d3e6775f7b474674d2b7d5e9c161d
naniya99 <naniya99@github.com>
2026-04-17 13:30:50 +0900

    Fix mic button with InkWell

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2630799791" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2873855018" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
