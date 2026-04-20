48151295ad50366c11b0762232beadd8239b3089
naniya99 <naniya99@github.com>
2026-04-20 15:52:36 +0900

    messagesProvide is changed to prevent reffuring results

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4146051478" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2105203259" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
